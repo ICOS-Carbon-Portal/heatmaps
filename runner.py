026aeac827593ba27a1c0e6af78df88c9e71d27a
zois <zoiszogop@gmail.com>
2026-03-11 15:39:39 +0100

    project - remove old code

--- runner.py
+++
@@ -1,6 +0,0 @@
-from src.heatmap import gimme_heatmaps
-from src.settings import Settings
-
-
-settings = Settings().settings
-gimme_heatmaps(settings)
--- src/constants/general_settings.py
+++
@@ -1,2 +0,0 @@
-YAML_SETTINGS = 'settings.yml'
-RAW_DATA_QUERY = 'src/constants/query_raw_data.txt'
